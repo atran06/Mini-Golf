d3df0ee39563a4e0252364b0538013f26c13caee
Arthur Tran <tran.art13@gmail.com>
2018-05-30 21:02:31 -0700

    Added new block

--- src/com/arthurtran/objects/woodBlock.java
+++ src/com/arthurtran/objects/woodBlock.java
@@ -2,33 +2,18 @@ package com.arthurtran.objects;
 
 import com.arthurtran.game.Objects;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
 
-import java.awt.*;
 import java.awt.geom.Rectangle2D;
 
-public class Barrier extends Objects {
+public class woodBlock extends Objects {
 
-    private Image blockTexture;
-
-    public Barrier(double x, double y, Enum ID) {
+    public woodBlock(double x, double y, Enum ID) {
         super(x, y, ID);
-
-        blockTexture = new Image(getClass().getResourceAsStream("/textures/block1.png"));
     }
 
     @Override
     public void draw(GraphicsContext g) {
-//        g.setFill(Color.gray(1));
-//        g.fillRect(x, y, 32, 32);
-//
-//        g.setLineWidth(1);
-//
-//        g.setStroke(Color.gray(0));
-//        g.strokeRect(x, y, 32, 32);
-
-        g.drawImage(blockTexture, x, y);
+        
     }
 
     @Override
@@ -38,7 +23,7 @@ public class Barrier extends Objects {
 
     @Override
     public Rectangle2D getBounds() {
-        return new Rectangle2D.Double(x, y, 32, 32);
+        return null;
     }
 
     @Override
