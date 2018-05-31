9abb9275f1be3b78196971edc2806402b361a11a
Arthur Tran <tran.art13@gmail.com>
2018-05-30 17:43:08 -0700

    Added barrier texture

--- src/com/arthurtran/objects/Barrier.java
+++ src/com/arthurtran/objects/Barrier.java
@@ -2,25 +2,34 @@ package com.arthurtran.objects;
 
 import com.arthurtran.game.Objects;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
+import java.awt.*;
 import java.awt.geom.Rectangle2D;
 
 public class Barrier extends Objects {
 
+    private Image blockTexture;
+
     public Barrier(double x, double y, Enum ID) {
         super(x, y, ID);
+
+
+        blockTexture = new Image(getClass().getResourceAsStream("/textures/block1.png"));
     }
 
     @Override
     public void draw(GraphicsContext g) {
-        g.setFill(Color.gray(1));
-        g.fillRect(x, y, 32, 32);
+//        g.setFill(Color.gray(1));
+//        g.fillRect(x, y, 32, 32);
+//
+//        g.setLineWidth(1);
+//
+//        g.setStroke(Color.gray(0));
+//        g.strokeRect(x, y, 32, 32);
 
-        g.setLineWidth(1);
-
-        g.setStroke(Color.gray(0));
-        g.strokeRect(x, y, 32, 32);
+        g.drawImage(blockTexture, x, y);
     }
 
     @Override
